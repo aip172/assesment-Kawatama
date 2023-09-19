c714941b0eb3303da583d1c7431f714dc7417b5b
Aip Sarif H <arif.ubeng172@.com>
2023-09-19 14:41:44 +0700

    menambah autentikasi dan memisahkan dari connection

--- auth.php
+++ auth.php
@@ -1,12 +1,33 @@
-<?php 
+<?php
 
-session_start();
-  $DB_HOST = 'localhost';
-  $DB_USER = 'root';
-  $DB_PASS = '';
-  $DB_NAME = 'auth';
+include('conn.php');
 
-$con = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
-if(mysqli_connect_errno()){
-  exit('Koneksi gagal. Error: '. mysqli_connect_error());
+if (!isset($_POST['user'], $_POST['pass'])) {
+  exit('Mohon isi data login');
 }
+
+if ($stmt = $con->prepare('SELECT id, pass FROM akun WHERE user = ?')) {
+  $stmt->bind_param('s', $_POST['user']);
+  $stmt->execute();
+  $stmt->store_result();
+
+  if ($stmt->num_rows > 0) {
+    $stmt->bind_result($id, $pass);
+    $stmt->fetch();
+
+    if ($_POST['pass'] === $pass) {
+      session_regenerate_id();
+      $_SESSION['loggedin'] = TRUE;
+      $_SESSION['nama'] = $_POST['user'];
+      $_SESSION['id'] = $id;
+      echo 'Welcome ' . $_SESSION['nama'] . '!';
+    } else {
+      echo 'Password salah!';
+    }
+  } else {
+    echo 'Username tidak terdaftar!';
+  }
+
+  $stmt->close();
+}
+
